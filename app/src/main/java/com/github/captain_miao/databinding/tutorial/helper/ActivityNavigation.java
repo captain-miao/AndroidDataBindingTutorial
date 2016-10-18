846e669b1ef157c838436a662ae539e72d008dfa
yanlu <caoqinmiao@gmail.com>
2016-10-18 18:20:55 +0800

    startActivitySafely

--- app/src/main/java/com/github/captain_miao/databinding/tutorial/helper/ActivityNavigation.java
+++ app/src/main/java/com/github/captain_miao/databinding/tutorial/helper/ActivityNavigation.java
@@ -1,5 +1,6 @@
 package com.github.captain_miao.databinding.tutorial.helper;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -33,6 +34,16 @@ public class ActivityNavigation {
 		return this;
 	}
 
+    /**
+     * @param request_code should >= 0, or no result will be returned to source activity.
+     *
+     * */
+   	public ActivityNavigation forResult(final int request_code) {
+   		if (! (mContext instanceof Activity)) throw new IllegalStateException("Only valid from Activity, but from " + mContext);
+   		mRequestCode = request_code;
+   		return this;
+   	}
+
     public boolean toUri(final String uri) {
 
         if (TextUtils.isEmpty(uri))
@@ -45,19 +56,41 @@ public class ActivityNavigation {
     public boolean toUri(final Uri uri) {
         final Intent intent = mIntent.setData(uri);
         Log.d(TAG, uri.toString());
-        try {
-            mContext.startActivity(intent);
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
+        if (isIntentSafe(intent)) {
+                if (mRequestCode >= 0) {
+                    ((Activity) mContext).startActivityForResult(intent, mRequestCode);
+                } else {
+                    mContext.startActivity(intent);
+                }
+                return true;
+        } else {
+            // 不合法的Url 也做个尝试
+            try {
+                if (mRequestCode >= 0) {
+                    ((Activity) mContext).startActivityForResult(intent, mRequestCode);
+                } else {
+                    mContext.startActivity(intent);
+                }
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
         }
     }
 
+    /**
+     * this method is from the official guide:
+     * http://developer.android.com/training/basics/intents/sending.html#StartActivity
+     * */
+    private boolean isIntentSafe(Intent intent) {
+        return !mContext.getPackageManager().queryIntentActivities(intent, 0).isEmpty();
+    }
 
 
 	private final Context mContext;
 	private final Intent mIntent;
+    private int mRequestCode = -1;
 
 	private static final String TAG = "ActivityNavigation";
 }
